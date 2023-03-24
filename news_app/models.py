b19471df6b122a2ae1bab326c2635bef62f447af
alinu1 <uwu1343@gmail.com>
2023-03-25 00:43:32 +0600

    first model and function

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='logo/')
+    about = models.TextField()
+    twitter = models.URLField(blank=True,null=True)
+    facebook = models.URLField(blank=True,null=True)
+    instagram = models.URLField(blank=True,null=True)
+    email = models.EmailField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройка'
+        verbose_name_plural = 'Настройки'
+
+class News(models.Model):
+    headline = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- news_app/views.py
+++ news_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    settings = Setting.objects.latest('id')
+    news  = News.objects.all()
+    context = {
+        'setting' : settings,
+        'news' : news
+    }
+    return render(request, 'index.html',context)
